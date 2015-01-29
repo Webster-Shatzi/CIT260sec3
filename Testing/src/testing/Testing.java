1481ca2a280547bd368a516586498f01a63cff1a
Shatzi Webster <>
2015-01-29 10:37:46 -0700

    testing again

--- Testing/src/testing/Testing.java
+++ Testing/src/testing/Testing.java
@@ -7,7 +7,7 @@ package testing;
 
 /**
  *
- * @author Shatzi
+ * @author ShatziWebster
  */
 public class Testing {
 
@@ -15,7 +15,7 @@ public class Testing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Testing");
+        System.out.println("Testing, testing.");
     }
     
 }
